60b52d98a559b501f1b2fdcec6147d68e679bc39
dmolinamesa01 <david.molina.m@estudiantat.upc.edu>
2023-05-31 18:53:58 +0200

    creada branca afegir llsitar coments

--- src/components/FilterForm.js
+++ src/components/FilterForm.js
@@ -1,3 +1,5 @@
+import React from 'react';
+
 export function FilterForm() {
   return (
     <form method="POST">
---
+++ src/screens/IssueScreen.js
@@ -0,0 +1,16 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+import '../styles/issue.css';
+import '../styles/common.css';
+import Header from '../components/Header';
+
+const IssueScreen = () => {
+  return (
+    <div>
+      <Header />
+
+    </div>
+  );
+};
+
+export default IssueScreen;
