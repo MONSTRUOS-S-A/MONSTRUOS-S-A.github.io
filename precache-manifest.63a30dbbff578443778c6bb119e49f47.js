9d71046cb44d70294cf32b61ea2ae1faa86846c6
Jorge_Borras <jorge.borras@estudiantat.upc.edu>
2023-06-01 21:50:29 +0200

    Updates

---
+++ precache-manifest.63a30dbbff578443778c6bb119e49f47.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "645fd48c5e4de5f59afb",
+    "url": "/issue-tracker-client/static/css/main.58120356.chunk.css"
+  },
+  {
+    "revision": "645fd48c5e4de5f59afb",
+    "url": "/issue-tracker-client/static/js/main.ce769adc.chunk.js"
+  },
+  {
+    "revision": "a11300315d91716b40fb",
+    "url": "/issue-tracker-client/static/js/runtime~main.43d77f01.js"
+  },
+  {
+    "revision": "282a7cee6eb0178c09a5",
+    "url": "/issue-tracker-client/static/js/2.231f0c1e.chunk.js"
+  },
+  {
+    "revision": "2cb1a2f85e156e5a832f",
+    "url": "/issue-tracker-client/static/js/3.b95fb9f1.chunk.js"
+  },
+  {
+    "revision": "b7b05f4972872ae5686ad5a2edff12af",
+    "url": "/issue-tracker-client/index.html"
+  }
+];
